3a59757ba6baea55c09b0c3869dc545995403c76
iskanderandrews <iskander.s.andrews@gmail.com>
2022-06-02 14:30:43 +0200

    build: practicing building a small webserver

---
+++ 1_programming_rust_book/src/chapter_2/example_3_b.rs
@@ -0,0 +1,35 @@
+// A Simple Web Server
+extern crate iron;
+extern crate router;
+#[macro_use] extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+fn main() {
+    let mut router = Router::new();
+
+    router.get("/", get_form, "root");
+    router.post("/gcd", post_gcd, "gcd");
+
+    println!("Serving on http://localhost:3000...");
+    Iron::new(router).http("localhost:3000").unwrap();
+}
+
+fn get_form(_request: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    response.set_mut(status::Ok);
+    response.set_mut(mime!(Text/Html; Charset=Utf8));
+    response.set_mut(r#"
+        <title>GCD Calculator</title>
+        <form action="/gcd" method="post">
+            <input type="text" name="n" />
+            <input type="text" name="n" />
+            <button type="submit"> Compute GCD </button>
+        </form>
+    "#);
+
+    Ok(response)
+}
