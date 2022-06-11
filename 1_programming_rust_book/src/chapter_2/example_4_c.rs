1f53c8be4090ec07e560694754b00b0655a6cbf8
iskanderandrews <iskander.s.andrews@gmail.com>
2022-06-11 06:48:45 +0200

    build: implementing a pixel to point functino

---
+++ 1_programming_rust_book/src/chapter_2/example_4_c.rs
@@ -0,0 +1,29 @@
+
+/// Given the row and column of a pixel in the outpu image, return the 
+/// correspoinding point on the complex plane. 
+/// 
+/// `bounds` is a pair giving the width and height of the image in pixels
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+/// The `upper_left` and `lower_right` parameters are points on the complex
+/// plane designating the are our image covers. 
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>
+                  lower_right: Complex<f64>) -> Complex<f64> 
+{
+    let (width, height) = (lower_right.re - upper_left.re, 
+                            upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        // Why subtraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
+    }
+}
+
+#[test]
+fn test_pixel_to_point() -> i32 {
+    assert_eq!(pixel_to_point((100, 100), (25, 75), 
+    Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0 }), Complex { re: -0.5, im: -0.5 })
+}
